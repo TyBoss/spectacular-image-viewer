96510026c8b8292752a9432f832f98d56b7fe9c2
Tyrone Brim <tyrone.brim@gmail.com>
2017-07-10 14:53:48 -0700

    initial draft of images api

--- app.js
+++ app.js
@@ -2,11 +2,35 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const pug = require('pug')
+const Flickr = require('flickrapi')
+
+const { promisify } = require('util')
+
+const authenticate = promisify(Flickr.tokenOnly)
+
+const flickrOptions = {
+  api_key: process.env.FLICKR_KEY,
+  secret: process.env.FLICKR_SECRET,
+  nobrowser: true,
+  silent: true,
+  noAPI: false,
+  progress: false
+}
 
 app.set('view engine', 'pug')
 app.set('views', './views')
+app.use('/static', express.static('dist'))
 
 app.get('/', (req, res) => res.render('index'))
+
+app.get('/images', async (req, res, next) => {
+  const flickrApi = await authenticate(flickrOptions)
+  const search = promisify(flickrApi.photos.search)
+
+  const results = await search({ text: 'sharingan' })
+  res.json(results.photos)
+  return
+})
 
 app.listen(9001, () => console.log('App is running at 9001!!!!'))
 
