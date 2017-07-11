448502b2ac45e121b9622e8269ab58716382ee42
Tyrone Brim <tyrone.brim@gmail.com>
2017-07-11 11:53:42 -0700

    add next and previous navigation on lightbox

--- dist/app.js
+++ dist/app.js
@@ -2,39 +2,71 @@ console.log('meh')
 
 const OK = 200;
 const BOOM = 500;
+const viewerState = {
+  images: [],
+  currentIdx: null
+}
 
-const letThereBeLight = (img) => {
+const letThereBeLight = (index) => {
+  viewerState.currentIdx = index
+  
   const lightBox = document.querySelector('#light-box')
   lightBox.className = lightBox.className === 'turn-off' ? 'turn-on' : 'turn-off'
   
+  setImageInBox(index)  
+}
+
+const setImageInBox = (index) => {
+  const img = viewerState.images[index]
   const imageTitle = document.querySelector('#light-box .img-title')
   imageTitle.innerText = img.title
 
   const imageView = document.querySelector('#light-box .image-viewer')
+  imageView.innerHTML = null
   const imagine  = document.createElement('img')
   imagine.src = img.url
   imageView.appendChild(imagine)
+
+  const moveToPrevious = document.querySelector('#light-box .previous')
+  const moveToNext = document.querySelector('#light-box .next')
+  
+  if (index === 0) {
+    moveToPrevious.classList.add('hide-me')
+  } else {
+    moveToPrevious.classList.remove('hide-me')
+    moveToPrevious.onclick = () => setImageInBox(index - 1)
+  }
+
+  if (index === viewerState.images.length - 1) {
+    moveToNext.classList.add('hide-me')
+  } else {
+    moveToNext.classList.remove('hide-me')
+    moveToNext.onclick = () => setImageInBox(index + 1)
+  }
 }
 
 const fetchImages = () => {
   const xmlHTTPRequest = new XMLHttpRequest()
   xmlHTTPRequest.open('GET', '/images', true)
   xmlHTTPRequest.onload = function () {
-    const imageGrid = document.querySelector('.image-grid')    
     if (this.status === OK) {
-      const images = JSON.parse(this.response)
-      
-      images.forEach((img, i) => {
-        const divContainer = document.createElement('div')
-	const image = document.createElement('img')
-	image.onclick = () => letThereBeLight(img)
-	image.src = img.url
-	divContainer.appendChild(image) 
-	imageGrid.appendChild(divContainer)
-      })
+      viewerState.images = JSON.parse(this.response)
+      renderImages()
     }
   }
   xmlHTTPRequest.send()
+}
+
+const renderImages = () => {
+  const imageGrid = document.querySelector('.image-grid')    
+  viewerState.images.forEach((img, i) => {
+    const divContainer = document.createElement('div')
+    const image = document.createElement('img')   
+    image.onclick = () => letThereBeLight(i)
+    image.src = img.url
+    divContainer.appendChild(image) 
+    imageGrid.appendChild(divContainer)
+  })
 }
 
 fetchImages()
